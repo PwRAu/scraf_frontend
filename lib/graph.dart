a05097599d79ec2b0c8ab6c26191fa2f988968a3
AleAioig <alessandro.aioig@gmail.com>
2021-02-10 12:27:19 +0100

    fix graph

--- lib/graph.dart
+++ lib/graph.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -8,90 +10,106 @@ class Graph extends StatefulWidget{
 
 class _Graph extends State<Graph>{
     Widget build(BuildContext context) {
-    //List<double> voti = new List(5);
+    List<double> voti = new List(5);
     double _width = MediaQuery.of(context).size.width;
     double _height= MediaQuery.of(context).size.height;
-    var voti = [4,5,1,5,7];
+    //var voti = [4,5,1,5,7];
     
-    /*
+    
     for(int i=0;i<voti.length;i++){
       var r = new Random();
       double n = r.nextDouble()*10;
       voti[i] = n;
     }
-    */
+    
 
     if(_width>650){
       return  Container(
-        //color: Colors.deepOrange,
-        padding: EdgeInsets.only(top:50),
-        width: _width*0.5,
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            SizedBox(
-              width: _width*0.4,
-              height: 450,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 25),
-                  Text(
-                    "Grafico prova",
-                    style: TextStyle(
-                      fontSize: 45,
-                    ),
-                  ),
-                  SizedBox(height: 25),
-                  Container(
-                    height: 300,
-                    width: _width*0.35,
-                    child: LineChart(
-                      LineChartData(
-                        minX: 0,
-                        maxX: 15,
-                        minY: 0,
-                        maxY: 10,
-                        //backgroundColor: Color(0xff4dd0e1),
-                        lineBarsData: [
-                          LineChartBarData(
-                            spots:[
-                              FlSpot(0,voti[0].toDouble()),
-                              FlSpot(4,voti[1].toDouble()),
-                              FlSpot(7,voti[2].toDouble()),
-                              FlSpot(11,voti[3].toDouble()),
-                              FlSpot(15,voti[4].toDouble()),
-                            ],
-                            isCurved: true,
-                            curveSmoothness: 0.5,
-                            colors: [Colors.lightBlue],
-                            
-                            colorStops: null,
-                            dotData: FlDotData(show: true),
-                            belowBarData: BarAreaData(
-                              show: true,
-                              colors: [Colors.lightBlue].map((e) => e.withOpacity(0.3)).toList(),
-                            ),
-                            preventCurveOverShooting: true,
+      padding: EdgeInsets.only(top:50),
+      width: _width*0.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:[
+          Container(
+            width: _width*0.4,
+            height: 450,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 25),
+                Text(
+                  "Grafico prova",
+                  style: TextStyle(
+                    color: Colors.lightBlue,
+                    fontSize: 45,
+                  )
+                ),
+                SizedBox(height: 25),
+                Container(
+                  height: 300,
+                  width: _width*0.35,
+                  child: LineChart(
+                    LineChartData(
+                      minX: 0,
+                      maxX: 15,
+                      minY: 0,
+                      maxY: 10,
+                      //backgroundColor: Color(0xff4dd0e1),
+                      lineBarsData: [
+                        LineChartBarData(
+                          spots:[
+                            FlSpot(0,voti[0]),
+                            FlSpot(4,voti[1]),
+                            FlSpot(7,voti[2]),
+                            FlSpot(11,voti[3]),
+                            FlSpot(15,voti[4]),
+                          ],
+                          isCurved: true,
+                          curveSmoothness: 0.5,
+                          colors: [Color(0xff23b6e6)],
                           
+                          colorStops: null,
+                          dotData: FlDotData(show: true),
+                          belowBarData: BarAreaData(
+                            show: true,
+                            colors: [Color(0xff23b6e6)].map((e) => e.withOpacity(0.3)).toList(),
                           ),
-                          
-                        ],
-                        borderData: FlBorderData(
-                          show: true,
-                          border: Border.all(color: Colors.blueGrey,width: 2),
+                          preventCurveOverShooting: true,
+                        
                         ),
                         
+                      ],
+                      borderData: FlBorderData(
+                        show: true,
+                        border: Border.all(color: const Color(0xff37434d),width: 2),
                       ),
                       
                     ),
+                    
                   ),
-                  
-                ]
+                ),
+              ]
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10)
               ),
-            )
-          ],
-        ),
-      );
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
     }else{
       return Container(
         width: _width,
