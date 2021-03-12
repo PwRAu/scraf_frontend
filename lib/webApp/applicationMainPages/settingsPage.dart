9b8609c6f691d0aacebe9f0289164c2aea53f9cf
AleAioig <alessandro.aioig@gmail.com>
2021-03-12 19:36:42 +0100

    fixPageWitdth v1.2.1

--- lib/webApp/applicationMainPages/settingsPage.dart
+++ lib/webApp/applicationMainPages/settingsPage.dart
@@ -3,57 +3,59 @@ import 'package:flutter/material.dart';
 
 import '../../global/config.dart';
 
-
-class SettingsPage extends StatefulWidget{  
-  _SettingsPage createState()=> _SettingsPage();
+class SettingsPage extends StatefulWidget {
+  _SettingsPage createState() => _SettingsPage();
 }
 
-class _SettingsPage extends State<SettingsPage>{
-  bool _light=false;
+class _SettingsPage extends State<SettingsPage> {
+  bool _light = false;
 
   @override
   Widget build(BuildContext context) {
     double _width = MediaQuery.of(context).size.width;
     double _height = MediaQuery.of(context).size.height;
-    
-    if(box.containsKey('theme')){
-      _light=box.get('theme');
-    }else{
-     _light=_light;
+
+    if (box.containsKey('theme')) {
+      _light = box.get('theme');
+    } else {
+      _light = _light;
     }
 
-    if(_width>650){
+    if (_width > 650) {
       return Flexible(
         flex: 1,
-        child:Container(
-          padding: EdgeInsets.only(top:50),
-          width: _width*0.6,
+        child: Container(
+          padding: EdgeInsets.only(top: 50),
+          width: _width * 0.6,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children:[
+            children: [
               Container(
-                width: _width*0.35,
+                width: _width * 0.35,
                 height: 500,
                 //color: Colors.grey[100],
                 child: Column(
-                  children:[
+                  children: [
                     Center(
-                      child:Column(
-                        children:[
+                      child: Column(
+                        children: [
                           Text(
                             "Impostazioni",
-                            style: TextStyle(fontSize:26),
+                            style: TextStyle(fontSize: 26),
                           ),
-                          SizedBox(height:25),
+                          SizedBox(height: 25),
                           Row(
-                            children:[
-                              Text("Dark theme",style: TextStyle(fontSize: 19)),
-                              Switch(value: _light,onChanged: (state){
-                                currentTheme.switchTheme();
-                                setState(() {
-                                  _light=state;
-                                });
-                              },
+                            children: [
+                              Text("Dark theme",
+                                  style: TextStyle(fontSize: 19)),
+                              Switch(
+                                value: _light,
+                                onChanged: (state) {
+                                  currentTheme.switchTheme();
+                                  setState(() {
+                                    _light = state;
+                                  });
+                                },
                               ),
                             ],
                           ),
@@ -67,30 +69,38 @@ class _SettingsPage extends State<SettingsPage>{
           ),
         ),
       );
-    }else{
+    } else {
       return Container(
         width: _width,
-        height: _height*0.85,
+        height: _height * 0.85,
         child: Column(
-          children:[
-            SizedBox(height:25),
+          children: [
+            SizedBox(height: 25),
             //Contiene la pagina
             Container(
-              width: _width*0.85,
-              height: _height*0.85,
+              width: _width * 0.85,
+              height: _height * 0.85,
               child: Column(
-                children:[
-                  Text("Settings",style: TextStyle(fontSize: 29),),
-                  SizedBox(height:25),
+                children: [
+                  Text(
+                    "Settings",
+                    style: TextStyle(fontSize: 29),
+                  ),
+                  SizedBox(height: 25),
                   Row(
-                    children:[
-                      Text("Dark theme",style: TextStyle(fontSize: 18),),
-                      Switch(value: _light,onChanged: (state){
-                        currentTheme.switchTheme();
-                        setState(() {
-                          _light=state;
-                        });
-                       },
+                    children: [
+                      Text(
+                        "Dark theme",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Switch(
+                        value: _light,
+                        onChanged: (state) {
+                          currentTheme.switchTheme();
+                          setState(() {
+                            _light = state;
+                          });
+                        },
                       ),
                     ],
                   ),
@@ -102,5 +112,4 @@ class _SettingsPage extends State<SettingsPage>{
       );
     }
   }
-  
 }
